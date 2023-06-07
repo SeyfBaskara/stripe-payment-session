2303d5918aa6da929706091697867c37697e5313
Seyf <seyfettin.baskara@gmail.com>
2023-06-07 17:01:58 +0200

    add config initializers

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/seyfBaskara/stripe-payment-session/initializers"
+
 )
 
 var (
@@ -11,18 +13,28 @@ var (
 )
 
 func init (){
+	_, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
 	server = gin.Default()
 }
 
 
 func main () {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
 
 	router := server.Group("api")
 	router.GET("/ping", func(c *gin.Context) {
 	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
+		"message": "ponggg",
 	  })
 	})
 	
-	log.Fatal(server.Run())
+	log.Fatal(server.Run(":" + config.ServerPort))
 }
